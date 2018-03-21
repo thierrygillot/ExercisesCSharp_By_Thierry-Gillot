82cdd7858040da87eeebc0307ba9e65982d5d936
Evoliris <Evoliris@PC401>
2018-03-21 13:33:43 +0100

    Add project files.

---
+++ ExercisesCSharp_By_Thierry Gillot/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesCSharp_By_Thierry_Gillot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Introduction - Print Hello World
+
+            //Console.WriteLine("Hello World");
+
+            #endregion
+            #region Print lines in console
+            //Print Hello in console and a second line with your firstname
+            //Console.WriteLine("Hello");
+            //Console.WriteLine("Thierry");
+            //Console.WriteLine("Hello\nThierry");
+            #endregion
+            #region Reading user input
+            //Ask favorite user color and always print "Mine too!" for answer
+            //Console.WriteLine("What's your favorite color ?");
+            //Console.ReadLine();
+            //Console.WriteLine("Mine too!");
+            #endregion
+            #region Sum of two numbers
+            //Use two variables int. One initialized at 13 and the second at 12
+            //Make the sum of the two variables and print the result
+            //int n1 = 13;
+            //int n2 = 12;
+            //Console.WriteLine(n1+n2);
+            #endregion
+            #region Division of two numbers
+            //Use two variables int. One initialized at 24 and the second at 5
+            //Make the sum of division 24/5 and print the result
+            //int n1 = 24;
+            //int n2 = 5;
+            //Console.WriteLine(n1/n2);
+            #endregion
+            #region Multiple operations
+            //Create a program to print the result of many operations
+            //Console.WriteLine(-6+3*5);
+            //Console.WriteLine((13-2)*7);
+            //Console.WriteLine((5+-2)*(20/10));
+            //Console.WriteLine((12+4)/(5-4));
+            #endregion
+            #region Multiply, using variables entries
+            //int n1;
+            //int n2;
+            //Console.WriteLine("Enter first number:");
+            //bool input1 = int.TryParse(Console.ReadLine(), out n1);
+            //Console.WriteLine("Enter second number:");
+            //bool input2 = int.TryParse(Console.ReadLine(), out n2);
+            //Console.WriteLine($"Result is {n1*n2}");
+            #endregion
+
+        }
+    }
+}
